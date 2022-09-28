e6cf9203d35e2779e05e8444bcb3feebe6734f7a
Sung Cho <sung.w.cho@protonmail.com>
2022-09-27 19:01:00 -0700

    test comments

--- whisper_experiments/tests/utils/test_string_utils.py
+++ whisper_experiments/tests/utils/test_string_utils.py
@@ -22,6 +22,9 @@ T = TextDiff
 LC = LineComparison
 
 
+# This test is just a sanity check for [Removed|Added|Modified]Line objects.
+# - Objects of the different types must be evaluted as not equal.
+# - Objects of same type and different underlying texts must be evaluated as not equal.
 @pytest.mark.parametrize(
     "text_diff_1, text_diff_2, equality",
     [
@@ -86,13 +89,18 @@ def test_text_diff_eq(text_diff_1: Line, text_diff_2: Line, equality: bool) -> N
     assert (T(text_diff_1) == T(text_diff_2)) == equality
 
 
+# Test word_differences() i.e. comparing line 1 and 2 as lists of words
 @pytest.mark.parametrize(
     "line_1, line_2, diff_words",
     [
         ("", "", []),
+        # The word 'a' exists in left line, not in right line
+        # so I expect the function to return RemovedWord('a')
         ("a", "", [RW("a")]),
         ("", "a", [AW("a")]),
         ("a", "b", [RW("a"), AW("b")]),
+        # The lines are similar enough to be determined as modified
+        # as opposed to completely removed or added
         (
             "ab",
             "abb",
@@ -105,7 +113,8 @@ def test_text_diff_eq(text_diff_1: Line, text_diff_2: Line, equality: bool) -> N
                 )
             ],
         ),
-        # word_differences() should not include unchanged words (e.g. "foobar")
+        # word_differences() should not include unchanged words (i.e. "foobar")
+        # so the result should be same as above test case
         (
             "ab foobar",
             "abb foobar",
@@ -126,10 +135,16 @@ def test_word_differences(line_1: str, line_2: str, diff_words: List[T]) -> None
     assert list(word_differences(words_1, words_2)) == list(map(T, diff_words))
 
 
+# Test text_differences() i.e.
+# 1. compare two text blobs as lines of text
+# 2. then compare the words in the removed/added/modified lines
 @pytest.mark.parametrize(
     "text_1, text_2, diff_lines",
     [
         ("", "", []),
+        # Left blob has a line with a single word 'a'
+        # that is 'removed' going to the right line.
+        # Thus, there is a RemovedLine('a'), which has a RemovedWord('a')
         ("a", "", [LC(T(RL("a")), [T(RW("a"))])]),
         ("", "a", [LC(T(AL("a")), [T(AW("a"))])]),
         ("a", "b", [LC(T(RL("a")), [T(RW("a"))]), LC(T(AL("b")), [T(AW("b"))])]),
@@ -159,6 +174,9 @@ def test_word_differences(line_1: str, line_2: str, diff_words: List[T]) -> None
                 )
             ],
         ),
+        # - First line is the same in both blobs so should be excluded in response
+        # - Second line is similar but changed from left to right (ModifiedLine)
+        # - and in the line there is 1 word that is changed (ModifiedWord)
         (
             "hello world\nhow are you",
             "hello world\nhow are yoou",
