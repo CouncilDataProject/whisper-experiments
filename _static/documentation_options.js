d6e82ed0ce5fa369eb5d677e566f1d1162778e32
evamaxfield <evamaxfieldbrown@gmail.com>
2022-10-11 03:08:56 +0000

    Deploying to gh-pages from @ CouncilDataProject/whisper-experiments@0994a65b0570f95603789202a96eeafd4619cc2b 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+gdd846b2',
+    VERSION: '0.1.dev1+g0994a65',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
